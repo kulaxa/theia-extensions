47a3e97b6c9848278bf5f10621f46140144b58c5
Mario Jalšovec <mario.jalsovec@mindsmiths.com>
2022-09-11 19:03:02 +0200

    updated ports widget to not open README on every reload

--- widget/src/browser/widget-contribution.ts
+++ widget/src/browser/widget-contribution.ts
@@ -11,6 +11,7 @@ import { Command, CommandRegistry } from '@theia/core/lib/common/command';
 import {FrontendApplicationStateService} from "@theia/core/lib/browser/frontend-application-state";
 import URI from "@theia/core/lib/common/uri";
 import {FileDialogService, SaveFileDialogProps} from "@theia/filesystem/lib/browser";
+import {FileService} from "@theia/filesystem/lib/browser/file-service";
 export const WidgetCommand: Command = { id: 'widget:command' };
 
 export const PortsWidgetCommand: Command = {
@@ -52,15 +53,31 @@ export class WidgetContribution extends AbstractViewContribution<PortsWidget> im
     @inject(OpenerService)
     protected readonly fileOpener!: OpenerService;
 
+    @inject(FileService)
+    protected readonly fileService!: FileService;
+
+    initialize() {
+        console.log("SLDJFKSDJFKSLDJFLKSDJFLKSDJFSDJ")
+    }
+
+
+
     async onStart(app: FrontendApplication): Promise<void> {
-    //     // open file explorer and attach ports widget
+         // open file explorer and attach ports widget
          this.stateService.reachedState('ready').then(
             () => {
-                super.openView({ toggle:true});
-                this.fileOpener.getOpener(
-                    new URI("/app/README.md")).then(opener => {
-                        opener.open(new URI("/app/README.md"));
-                    });
+                const theia_startup_file="/root/.theia/ide_started"
+                this.fileService.exists(new URI(theia_startup_file)).then(result =>{
+                    if(!result){
+                        super.openView({ toggle:true});
+                        this.fileOpener.getOpener(
+                            new URI("/app/README.md")).then(opener => {
+                            opener.open(new URI("/app/README.md"));
+                        });
+                        this.fileService.createFile(new URI(theia_startup_file))
+                    }
+                })
+
             })
     }
 
